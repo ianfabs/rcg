57db0277354ab562960471ec275bada2caf042b2
Ian Fabs <ianfabs@outlook.com>
2020-02-16 19:06:50 -0500

    Rewrote CLI to fix issues

--- cli.ts
+++ cli.ts
@@ -1,13 +1,10 @@
 import { parse } from "https://deno.land/std/flags/mod.ts";
-import "https://deno.land/x/eaisercolors/mod.ts"
 
-const args = parse(Deno.args.slice(1, Deno.args.length));
-
-const print = (text: string) => {
-  let encoder = new TextEncoder();
-  let textBuffer = new Deno.Buffer(encoder.encode(text));
-  Deno.copy(Deno.stdout, textBuffer)
-}
+// Have to slice because deno install command auto-adds `--` to the command
+const args = parse(Deno.args.slice(1, Deno.args.length), {
+  boolean: ["format", "f", "help", "h"]
+});
+const COLOR_SCHEMES = ["hex", "hexa", "rgb", "rgba", "hsl", "hsla"];
 
 const HELP_MSG = `
   rcg CLI ~ created by Ian Fabs<ian@fabs.dev>
@@ -25,25 +22,25 @@ const HELP_MSG = `
     $ rcg --format
     #1b69af
 `;
-const COLOR_SCHEME_TYPES = ["hex", "hexa", "rgb", "rgba", "hsl", "hsla"];
 
-if (args._.includes("help") || args.help) {
-  print(HELP_MSG + "\n");
+const helpRequested: boolean = args._.includes("help") || args.help || args.h;
+
+if (helpRequested) {
+  console.log(HELP_MSG);
+  Deno.exit(0);
 } else {
-  let userColorScheme: string;
-  let color: any;
-
-  if (args._[0] != "help") userColorScheme = args._[0];
-  else userColorScheme = args._[1];
-
-  if (userColorScheme == "" || userColorScheme == undefined) {
-    color = await import("./hex.ts");
-  } else if (COLOR_SCHEME_TYPES.includes(userColorScheme)) {
-    color = await import(`./${userColorScheme}.ts`);
+  let userColorScheme: string = args._[0] ?? "hex";
+  if (COLOR_SCHEMES.includes(userColorScheme)) {
+    let importPath = `./${userColorScheme}.ts`;
+    let fnImport = await import(importPath);
+    // Should probably just use default export
+    // const fn = (fnImport?.[userColorScheme] ?? fnImport.default);
+    const fn = fnImport.default;
+    const shouldFormat: boolean = args.format || args.f || false;
+    console.log(fn(shouldFormat));
+    Deno.exit(0);
   } else {
-    print("Error:".red() + ` ${userColorScheme} is not a valid color scheme. \n`)
+    console.error(`RCG: Invalid color scheme '${userColorScheme}'`);
+    Deno.exit(1);
   }
-  const result = color?.default(args.format ?? args.f);
-  print(result?.join?.(", ") ?? result);
 }
-Deno.stdin.close();
